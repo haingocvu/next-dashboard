87570594a87d5e4df6e31e1cc698b17cf28ddf09
haivn <haivn@guide.inc>
2024-10-17 12:01:00 +0700

    chapter 10

--- app/dashboard/layout.tsx
+++ app/dashboard/layout.tsx
@@ -1,5 +1,8 @@
 import React from "react";
 import SideNav from "@/app/ui/dashboard/sidenav";
+
+export const experimental_ppr = true;
+
 function DashboardLayout({ children }: { children: React.ReactNode }) {
   return (
     <div className="flex h-screen flex-col md:flex-row md:overflow-hidden">
