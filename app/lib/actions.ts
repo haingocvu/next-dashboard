4976dcdbe9fd8b9cfd4ebd5b4cc4daeefc46672e
haivn <haivn@guide.inc>
2024-10-22 15:34:32 +0700

    chapter 15

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -1,5 +1,7 @@
 "use server";
+import { signIn } from "@/auth";
 import { sql } from "@vercel/postgres";
+import { AuthError } from "next-auth";
 import { revalidatePath } from "next/cache";
 import { redirect } from "next/navigation";
 import { z } from "zod";
@@ -103,6 +105,26 @@ export async function deleteInvoice(id: string) {
   }
 
   revalidatePath("/dashboard/invoices");
+}
+
+export async function authenticate(
+  prevState: string | undefined,
+  formData: FormData
+) {
+  try {
+    await signIn("credentials", formData);
+  } catch (error) {
+    if (error instanceof AuthError) {
+      switch (error.type) {
+        case "CredentialsSignin":
+          return "Invalid Credentials.";
+        default:
+          return "Someting went wrong.";
+      }
+    }
+
+    throw error;
+  }
 }
 
 export type State = {
